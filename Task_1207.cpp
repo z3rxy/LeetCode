257aadc66a10fe676a4f3b49dcce767f010be690
z3rxy <pa.volkogonov@gmail.com>
2023-10-05 01:44:25 +0300

    First task

---
+++ Task_1207.cpp
@@ -0,0 +1,22 @@
+int speedup = [] {ios::sync_with_stdio(0); cin.tie(0); return 0; } ();
+class Solution {
+public:
+	bool uniqueOccurrences(vector<int>& arr) {
+		map<int, int> ocurr;
+		unordered_set<int> set;
+
+		for (int i = 0; i < arr.size(); i++) {
+			if (!ocurr.count(arr[i])) {
+				ocurr[arr[i]] = 1;
+			}
+			else {
+				ocurr[arr[i]] = ocurr[arr[i]] + 1;
+			}
+		}
+
+		for (auto num : ocurr) {
+			if (!set.insert(num.second).second) return false;
+		}
+		return true;
+	}
+};
