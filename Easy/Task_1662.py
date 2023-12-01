f21b03dc29aa937cd4092f712b5f4e7d5c7a80ba
z3rxy <pa.volkogonov@gmail.com>
2023-12-01 12:57:25 +0300

    Added task №1662 from LeetCode

---
+++ Easy/Task_1662.py
@@ -0,0 +1,5 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        string1 = "".join(word1)
+        string2 = "".join(word2)
+        return string1 == string2
